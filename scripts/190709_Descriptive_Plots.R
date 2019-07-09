5718687db1c00c732bb2bc0815b82b16a300f43c
Jim Hughes <jhhughes256@gmail.com>
2019-07-09 14:22:06 -0400

    Finished up descriptive plots

--- scripts/190709_Descriptive_Plots.R
+++ scripts/190709_Descriptive_Plots.R
@@ -50,14 +50,19 @@
   bayes_tb <- readr::read_rds("output/vanc_regimen_bayes_fix.rds") %>%
     dplyr::select(ID, bayes) %>% tidyr::unnest() %>%
     tibble::add_column(method = "bayes")
+  
+  baynl_tb <- readr::read_rds("output/vanc_regimen_bayes_newload.rds") %>%
+    dplyr::select(ID, bayes) %>% tidyr::unnest() %>%
+    tibble::add_column(method = "baynl")
 
 # Bind into plot dataset and calculate cumulative dose
-  plot_tb <- dplyr::bind_rows(sagov_tb, auc24_tb, bayes_tb) %>%
+  plot_tb <- dplyr::bind_rows(sagov_tb, auc24_tb, bayes_tb, baynl_tb) %>%
     dplyr::mutate(methodf = factor(method)) %>%
     dplyr::mutate(CRCLf = factor(CRCL))
   levels(plot_tb$methodf) <- c(
     "Proportional TDM",
     "Model-Based TDM",
+    "Model-Based w/ Loading Dose",
     "SA Health Guidelines"
   )  # set levels
   levels(plot_tb$CRCLf) <- c(
@@ -98,7 +103,7 @@
   
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Evaluation Plots
-  palette <- with(cbPalette, c(red, blue, green))
+  palette <- with(cbPalette, c(red, blue, pink, green))
 
 # Trough Concentration-time plots
   p <- NULL
@@ -142,6 +147,20 @@
   # p <- p + coord_cartesian(xlim = c(0, 168), ylim = NULL)
   p1_bayes <- p + facet_wrap(~CRCLf)
   p1_bayes
+  
+  p <- NULL
+  p <- ggplot(data = dplyr::filter(plot_tb, method == "baynl"))
+  p <- p + ggtitle("Concentration Time Profile - Model-Based w/ Loading Dose")
+  p <- p + stat_summary(aes(x = time, y = IPRE), geom = "line", fun.y = median,
+    colour = cbPalette$pink, size = 1)
+  p <- p + stat_summary(aes(x = time, y = IPRE), geom = "ribbon",
+    fun.ymin = CI90lo,  fun.ymax = CI90hi, fill = cbPalette$pink, 
+    size = 1, alpha = 0.25)
+  p <- p + scale_x_continuous("\nTime (hours)", breaks = 0:7*24)
+  p <- p + scale_y_continuous("Vancomycin Concentration (mg/L)\n")
+  # p <- p + coord_cartesian(xlim = c(0, 168), ylim = NULL)
+  p1_baynl <- p + facet_wrap(~CRCLf)
+  p1_baynl
   
   p <- NULL
   p <- ggplot(data = plot_tb)
@@ -202,6 +221,21 @@
   p2_bayes <- p + facet_wrap(~CRCLf)
   p2_bayes
   
+  p <- NULL
+  p <- ggplot(data = dplyr::filter(auc_target_bycrcl, method == "baynl"))
+  p <- p + ggtitle("Probability of Target Attainment - Model-Based w/ Loading Dose")
+  p <- p + geom_line(aes(x = time, y = gt400*100),
+    size = 1, colour = cbPalette$green)
+  p <- p + geom_line(aes(x = time, y = gt700*100),
+    size = 1, colour = cbPalette$red)
+  p <- p + geom_line(aes(x = time, y = gt400lt700*100),
+    size = 1, colour = cbPalette$blue)
+  p <- p + scale_x_continuous("\nTime (days)", breaks = 0:7)
+  p <- p + scale_y_continuous("Probability of Target Attainment (%)\n")
+  p <- p + coord_cartesian(xlim = NULL, ylim = c(0, 100))
+  p2_baynl <- p + facet_wrap(~CRCLf)
+  p2_baynl
+  
 # Plot % patients with AUC within Target Range
   p <- NULL
   p <- ggplot(data = dplyr::filter(auc_target, method == "sagov"))
@@ -246,8 +280,26 @@
   p3_bayes
   
   p <- NULL
+  p <- ggplot(data = dplyr::filter(auc_target, method == "baynl"))
+  p <- p + ggtitle("Probability of Target Attainment - Model-Based w/ Loading Dose")
+  p <- p + geom_line(aes(x = time, y = gt400*100),
+    size = 1, colour = cbPalette$green)
+  p <- p + geom_line(aes(x = time, y = gt700*100),
+    size = 1, colour = cbPalette$red)
+  p <- p + geom_line(aes(x = time, y = gt400lt700*100),
+    size = 1, colour = cbPalette$blue)
+  p <- p + scale_x_continuous("\nTime (days)", breaks = 0:7)
+  p <- p + scale_y_continuous("Probability of Target Attainment (%)\n")
+  p3_baynl <- p + coord_cartesian(xlim = NULL, ylim = c(0, 100))
+  p3_baynl
+  
+  auc_target$methodf <- relevel(auc_target$methodf, "Model-Based TDM")
+  auc_target$methodf <- relevel(auc_target$methodf, "Proportional TDM")
+  auc_target$methodf <- relevel(auc_target$methodf, "SA Health Guidelines")
+  
+  p <- NULL
   p <- ggplot(data = auc_target)
-  p <- p + ggtitle("Probability of Target Attainment - Model-Based TDM")
+  p <- p + ggtitle("Probability of Target Attainment - TDM Method Comparison")
   p <- p + geom_line(aes(x = time, y = gt400*100),
     size = 1, colour = cbPalette$green)
   p <- p + geom_line(aes(x = time, y = gt700*100),
@@ -258,7 +310,9 @@
   p <- p + scale_y_continuous("Probability of Target Attainment (%)\n")
   p <- p + coord_cartesian(xlim = NULL, ylim = c(0, 100))
   p3_all <- p + facet_wrap(~ methodf, nrow = 1)
+  p3_all_square <- p + facet_wrap(~ methodf, nrow = 2)
   p3_all
+  p3_all_square
   
   p1_sagov
   ggsave("output/CvT_sagov.png", width = 8, height = 6)
@@ -267,6 +321,8 @@
   p1_bayes
   ggsave("output/CvT_bayes.png", width = 8, height = 6)
   # ggsave("output/CvT_bayes_addsamp.png", width = 8, height = 6)
+  p1_baynl
+  ggsave("output/CvT_bayes_load.png", width = 8, height = 6)
   p1_all
   ggsave("output/CvT_all.png", width = 8, height = 6)
   # ggsave("output/CvT_all_addsamp.png", width = 8, height = 6)
@@ -277,6 +333,8 @@
   p2_bayes
   ggsave("output/AUC_bycrcl_bayes.png", width = 8, height = 6)
   # ggsave("output/AUC_bycrcl_bayes_addsamp.png", width = 8, height = 6)
+  p2_baynl
+  ggsave("output/AUC_bycrcl_bayes_load.png", width = 8, height = 6)
   p3_sagov
   ggsave("output/AUCtarget_sagov.png", width = 8, height = 6)
   p3_auc24
@@ -284,7 +342,11 @@
   p3_bayes
   ggsave("output/AUCtarget_bayes.png", width = 8, height = 6)
   # ggsave("output/AUCtarget_bayes_addsamp.png", width = 8, height = 6)
+  p3_baynl
+  ggsave("output/AUCtarget_bayes_load.png", width = 8, height = 6)
   p3_all
   ggsave("output/AUCtarget_all.png", width = 8, height = 6)
   # ggsave("output/AUCtarget_all_addsamp.png", width = 8, height = 6)
+  p3_all_square
+  ggsave("output/AUCtarget_all_square.png", width = 8, height = 6)
   
